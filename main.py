9bdf4579647d97b3905549b59b2e460f4496da02
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2024-08-08 06:30:12 +0000

    feat: update options
    Signed-off-by: GitHub <noreply@github.com>

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class ChromeDriver:
     def __enter__(self):
         options = webdriver.ChromeOptions()
+        options.add_argument("--disable-background-networking")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--disable-default-apps")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-extensions")
+        options.add_argument('--disable-features=DownloadBubbleV2')
+        options.add_argument("--disable-features=Translate")
         options.add_argument('--headless')
+        options.add_argument("--hide-scrollbars")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--incognito")
+        options.add_argument("--mute-audio")
+        options.add_argument("--no-default-browser-check")
         options.add_argument('--no-sandbox')
-        options.add_argument('--user-agent=selenium')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--no-zygote')
-        options.add_argument('--single-process')
+        options.add_argument("--propagate-iph-for-testing")
+        options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
         self.driver = webdriver.Chrome(options=options)
         self.driver.set_window_size(1920, 1080)
         return self.driver
@@ -24,5 +32,6 @@ def main():
     with ChromeDriver() as driver:
         driver.get('https://www.google.com/')
         print(driver.title)
+
 
 main()
