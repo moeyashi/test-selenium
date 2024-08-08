1a1635f37fd3dd8ca159cc726e5c3157b3cf064a
Ren Adachi <49895682+moeyashi@users.noreply.github.com>
2024-08-08 05:10:10 +0000

    chore: ci設定
    Signed-off-by: Ren Adachi <49895682+moeyashi@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import time
-from datetime import datetime
-import psutil
-import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 
@@ -16,7 +12,7 @@ class ChromeDriver:
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--no-zygote')
         options.add_argument('--single-process')
-        self.driver = webdriver.Chrome(executable_path='chromedriver', options=options)
+        self.driver = webdriver.Chrome(options=options)
         self.driver.set_window_size(1920, 1080)
         return self.driver
 
@@ -24,29 +20,9 @@ class ChromeDriver:
         self.driver.quit()
 
 
-def print_all_processes():
-    for proc in psutil.process_iter(['pid', 'name']):
-        print(proc.info)
-
-
 def main():
-    started_at = datetime.now()
-    print(started_at)
     with ChromeDriver() as driver:
         driver.get('https://www.google.com/')
         print(driver.title)
-        loop_count = 1
-        while True:
-            driver.execute_script("window.open('https://example.com/');")
-            driver.switch_to.window(driver.window_handles[-1])
-            print(loop_count)
-            loop_count += 1
-            print(f"{datetime.now() - started_at}秒経過")
-            print(driver.title)
-            print_all_processes()
-            time.sleep(3)
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
-
 
 main()
